1c21807fd6fb9af0edde96ecfdd9ca77399c82f2
Aurelien-Be <abeveraggi@netcourrier.com>
2022-06-01 13:15:09 +0300

    update

--- teacherwebsite/settings.py
+++ teacherwebsite/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv
+import environ 
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = getenv("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY_SITE")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
